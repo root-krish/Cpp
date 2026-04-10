4c981cef80bf9f4f5f708ad60569407dd701b788
root-krish <krishnendu6080@gmail.com>
2026-04-10 18:54:45 +0530

    Fixed Waiting Time Error

--- Process-Management/fcfs.c
+++ Process-Management/fcfs.c
@@ -14,23 +14,24 @@ int main(){
     WaitingTime[0]=0;
     int TotalWaitingTime=0;
     for(i=1;i<n;i++){
+        WaitingTime[i]=0;
         WaitingTime[i]=brustTime[i-1]+WaitingTime[i-1];
         TotalWaitingTime=TotalWaitingTime+WaitingTime[i];
     }
     //calculation For Turnaround Time
     int TotalTurnAroundTime=0;
     for(i=0;i<n;i++){
+        TurnAroundTime[i]=0;
         TurnAroundTime[i]=brustTime[i]+WaitingTime[i];
         TotalTurnAroundTime=TotalTurnAroundTime+TurnAroundTime[i];        
     }
     //Avarage Time 
     float AverageWaitingTime=TotalWaitingTime/n;
-    float AverageTurnAroundTime=0;
-    AverageTurnAroundTime= (float)TotalTurnAroundTime/n;
+    float AverageTurnAroundTime=(float)TotalTurnAroundTime/n;
     //Display all
-    printf("Process Brust Waiting_Time TurnAround_Time ");
+    printf("Process BrustTime Waiting_Time TurnAround_Time ");
     for(i=0;i<n;i++){
-        printf("\n%7d %5d %7d %3d",process[i],brustTime[i],WaitingTime[i],TurnAroundTime[i]);
+        printf("\n%5d %6d %10d %14d",process[i],brustTime[i],WaitingTime[i],TurnAroundTime[i]);
     }
     printf("\nTotal Waiting Time=%d",TotalWaitingTime);
     printf("\nAvrage Turnarund Time=%f",AverageTurnAroundTime);
